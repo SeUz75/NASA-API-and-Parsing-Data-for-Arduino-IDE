42a44758b0424f283f21d7e9d31421676042949a
SeUz75 <seuz752@gmail.com>
2024-11-06 11:39:54 +0200

    java 11 -> 23

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,8 +1,7 @@
 package com.example; 
 import com.example.MoonDataDlient;
-
 import main.java.com.example.MoonDataClient;
-
+import com.fazecast.jSerialComm.SerialPort;
 
 /**
  * Hello world!
@@ -14,7 +13,24 @@ public class App
     {
         
         MoonDataClient client = new MoonDataClient();
-        System.out.println(client.findAll());
+        //System.out.println(client.findAll());
+
+        String moonposition = client.findAll();
+
+        System.out.println(moonposition);
+
+        // 
+        // Replace with your Arduino's COM port(Like "COM3" on Windows,"/dev/ttyUSB0" on Linux)
+        SerialPort comPort = SerialPort.getCommPort("COM3");
+        comPort.setBaudrate(9600);
+        if(comPort.openPort()){
+            System.out.println("Port is OPEN !");
+        }else{
+            System.out.println("Failed to open");
+            return;
+        }
+
+
 
     }
 }
